c8f2c7f6bb4f15db98d282faee8cf95af1cf3ece
Promise Das <dpromisingdev@gmail.com>
2020-08-09 19:57:41 +0600

    Assignment Completion

--- assignment.js
+++ assignment.js
@@ -14,7 +14,23 @@ function woodCalculator(chair, table, bed) {
 
 /* *Task 3: Calculationg bricks. */
 function brickCalculator(floor) {
-    
+    const brickPerFeet = 1000; 
+    let totalBricks = 0;
+    if (floor < 0) {
+        return "Floor number can't be negative";
+    } else if (floor <= 10) {
+        return (floor * 15) * brickPerFeet;
+    } else if (floor <= 20) {
+        const firstTenFloor = (10 * 15) * brickPerFeet;
+        const remainingFloor = (10 * 12) * brickPerFeet;
+        return firstTenFloor + remainingFloor;
+    } else {
+        const firstTenFloor = (10 * 15) * brickPerFeet;
+        const nextTenFloor = (10 * 12) * brickPerFeet;
+        const remainingFloor = (floor - 20) * 10 * brickPerFeet;
+        return firstTenFloor + nextTenFloor + remainingFloor;
+    }
+    return totalBricks;
 }
 
 /* *Task 4: Finding the smallest friend. */
